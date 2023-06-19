cd60f08b8def0d79ecd55688b875ea7087346fc5
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-06-19 18:57:05 +0000

    first commit Ruben branch

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Login} from "./login"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+		<button className="primary">Prueba</button>
 };
