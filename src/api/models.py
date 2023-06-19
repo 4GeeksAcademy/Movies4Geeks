78f22c51a038e32f5801444b1d20746d278e5ce2
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2023-06-19 19:23:03 +0000

    carousel and models

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    nickname = db.Column(db.String(120), unique=True, nullable=False)
+    birthday = db.Column(db.String(80), unique=False, nullable=True)
+    avatar = db.Column(db.String(80), unique=False, nullable=True)
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +19,94 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "nickname": self.nickname,
             "email": self.email,
+            "birthday": self.birthday,
+            "avatar": self.avatar
             # do not serialize the password, its a security breach
+        }
+    
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_title = db.Column(db.String(120), unique=False, nullable=False)
+    overview = db.Column(db.String(600), unique=False, nullable=False)
+    poster_path = db.Column(db.String(120), unique=False, nullable=False)
+    release_date = db.Column(db.String(120), unique=False, nullable=False)
+    backdrop_path = db.Column(db.String(120), unique=False, nullable=False)
+    gender_ids = db.Column(db.Integer, unique=False, nullable=False) # -----------------Preguntar Marcos
+    trailer = db.Column(db.String(120), unique=False, nullable=False) # -----------------Preguntar Marcos
+    image = db.Column(db.String(120), unique=False, nullable=False) # -----------------Preguntar Marcos
+
+    def __repr__(self):
+        return f'<Movie {self.original_title}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "original_title": self.original_title,
+            "poster_path": self.poster_path,
+            "release_date": self.release_date,
+            "backdrop_path": self.backdrop_path,
+            "gender_ids": self.gender_ids,
+            "trailer": self.trailer,
+            "image": self.image
+        }
+    
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    value = db.Column(db.Integer, unique=False, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    review_id = db.Column(db.Integer, db.ForeignKey('review.id'))
+    
+
+    def __repr__(self):
+        return f'<Score {self.value}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "value": self.value,
+            "movie_id": self.movie_id,
+            "review_id": self.review_id
+        }
+    
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    title = db.Column(db.String(120), unique=False, nullable=False)
+    text = db.Column(db.String(1200), unique=False, nullable=False)
+
+    
+
+    def __repr__(self):
+        return f'<Score {self.value}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "title": self.title,
+            "text": self.text,
+        }
+    
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    review_id = db.Column(db.Integer, db.ForeignKey('review.id'))
+    like = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Like {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "review": self.review,
+            "like": self.like,
         }
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,6 +13,88 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
+			],
+			upcoming: [
+				{
+					adult: false,
+					backdrop_path: "/nGxUxi3PfXDRm7Vg95VBNgNM8yc.jpg",
+					genre_ids: [
+						28,
+						12,
+						16,
+						878
+					],
+					id: 569094,
+					original_language: "en",
+					original_title: "Spider-Man: Across the Spider-Verse",
+					overview: "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
+					popularity: 2471.835,
+					poster_path: "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
+					release_date: "2023-05-31",
+					title: "Spider-Man: Across the Spider-Verse",
+					video: false,
+					vote_average: 8.8,
+					vote_count: 1285
+				},
+				{
+					adult: false,
+					backdrop_path: "/8FhKnPpql374qyyHAkZDld93IUw.jpg",
+					genre_ids: [
+						9648,
+						53,
+						878
+					],
+					id: 536437,
+					original_language: "en",
+					original_title: "Hypnotic",
+					overview: "A detective becomes entangled in a mystery involving his missing daughter and a secret government program while investigating a string of reality-bending crimes.",
+					popularity: 2137.534,
+					poster_path: "/3IhGkkalwXguTlceGSl8XUJZOVI.jpg",
+					release_date: "2023-05-11",
+					title: "Hypnotic",
+					video: false,
+					vote_average: 6.6,
+					vote_count: 166
+				},
+				{
+					adult: false,
+					backdrop_path: "/9NgtktUFLm9cnFDFaekx2ROh84f.jpg",
+					genre_ids: [
+						28,
+						12,
+						878
+					],
+					id: 667538,
+					original_language: "en",
+					original_title: "Transformers: Rise of the Beasts",
+					overview: "When a new threat capable of destroying the entire planet emerges, Optimus Prime and the Autobots must team up with a powerful faction known as the Maximals. With the fate of humanity hanging in the balance, humans Noah and Elena will do whatever it takes to help the Transformers as they engage in the ultimate battle to save Earth.",
+					popularity: 1751.386,
+					poster_path: "/gPbM0MK8CP8A174rmUwGsADNYKD.jpg",
+					release_date: "2023-06-06",
+					title: "Transformers: Rise of the Beasts",
+					video: false,
+					vote_average: 7.4,
+					vote_count: 260
+				},
+				{
+					adult: false,
+					backdrop_path: "/9t0tJXcOdWwwxmGTk112HGDaT0Q.jpg",
+					genre_ids: [
+						27,
+						53
+					],
+					id: 890771,
+					original_language: "en",
+					original_title: "The Black Demon",
+					overview: "Oilman Paul Sturges' idyllic family vacation turns into a nightmare when they encounter a ferocious megalodon shark that will stop at nothing to protect its territory. Stranded and under constant attack, Paul and his family must somehow find a way to get his family back to shore alive before it strikes again in this epic battle between humans and nature.",
+					popularity: 1187.323,
+					poster_path: "/uiFcFIjig0YwyNmhoxkxtAAVIL2.jpg",
+					release_date: "2023-04-26",
+					title: "The Black Demon",
+					video: false,
+					vote_average: 6.5,
+					vote_count: 162
+				}
 			]
 		},
 		actions: {
@@ -46,6 +128,38 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			getUpcoming: async() => {
+				var myHeaders = new Headers();
+				myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGYxMDkyNDZkNzUxYmJhYjNmMTQzMGNlYzNmYmU0NCIsInN1YiI6IjY0ODgxODY1ZDJiMjA5MDBjYTIxMTg2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RiM24dMvTMZi652gXFQnpguE7dT8yYex5ZsTaY3OjJw");
+
+				var requestOptions = {
+				method: 'GET',
+				headers: myHeaders,
+				redirect: 'follow'
+				};
+
+				const resp = await fetch("https://api.themoviedb.org/3/movie/upcoming", requestOptions)
+				const data = await resp.json()
+				console.log(data.results)
+				setStore({ upcoming: data.results })
+				
+			},
+			getTopRated: async() => {
+				var myHeaders = new Headers();
+				myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGYxMDkyNDZkNzUxYmJhYjNmMTQzMGNlYzNmYmU0NCIsInN1YiI6IjY0ODgxODY1ZDJiMjA5MDBjYTIxMTg2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RiM24dMvTMZi652gXFQnpguE7dT8yYex5ZsTaY3OjJw");
+
+				var requestOptions = {
+				method: 'GET',
+				headers: myHeaders,
+				redirect: 'follow'
+				};
+
+				const resp = await fetch("https://api.themoviedb.org/3/movie/top_rated", requestOptions)
+				const data = await resp.json()
+				console.log(data.results.length)
+				//setStore({ upcoming: data.results })
+				
 			}
 		}
 	};
