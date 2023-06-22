d442641f2c8bec382fbcee27282d0bc2859fabe5
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-06-22 15:47:55 +0000

    creo componente register para aplicar funciones en el login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,21 +1,42 @@
 import React from "react";
 import "../../styles/login.css";
+import { register } from "./register";
 
 export const Login = () => {
     return (
-            <div className="container">
-                <div className="caja-trasera">
-                    <div className="caja-trasera-login">
+    <main>
+            <div className="container__todo">
+                <div className="caja__trasera">
+                    <div className="caja__trasera__login">
                         <h3>¿Ya tienes una cuenta?</h3>
-                        <p>Inicia la sesión para entrar en la pagina</p>
+                        <p>Inicia sesión para entrar en la página</p>
                         <button id="btn__iniciar-sesion">Iniciar sesión</button>
                     </div>
-                    <div className="caja-trasera-register">
+                    <div className="caja__trasera-register">
                         <h3>¿Aún no tienes una cuenta?</h3>
-                        <p>Registrate y disfruta de las ventajas</p>
-                        <button id="btn__iniciar-sesion">Iniciar sesión</button>
+                        <p>Regístrate y disfruta de las ventajas</p>
+                        <button id="btn__registrarse">Registrarse</button>
                     </div>
                 </div>
+                {/* formulario de login y registro */}
+                <div className="container__login-register">
+                    <form action="" className="formulario__login">
+                        <h2>Iniciar Sesión</h2>
+                        <input type="text" placeholder="Correo Electrónico" /><br/>
+                        <input type="password" placeholder="Contraseña" /><br/>
+                        <button>Entrar</button>
+                    </form>
+                    <form action="" className="formulario__register">
+                        <h2>Regístrate</h2>
+                        <input type="text" placeholder="Nombre completo" /><br/>
+                        <input type="text" placeholder="Correo Electrónico" /><br/>
+                        <input type="text" placeholder="Nickname" /><br/>
+                        <input type="password" placeholder="Contraseña" />
+                        <button>Registrarse</button>
+                    </form>
+                </div>
             </div>
+            <register /> 
+    </main>
     );
 }
---
+++ src/front/js/pages/register.js
@@ -0,0 +1,20 @@
+document.getElementById("btn__registra")
+
+
+// Declaración de variables
+var container_login_register = document.querySelector(".container__login-register");
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var caja_trasera_login = document.querySelector(".caja_trasera-login");
+var caja_trasera_register = document.querySelector(".caja_trasera-register");
+
+
+export function register() {
+    formulario_register.style.display = "block";
+    container_login_register.style.left = "410px";
+    formulario_login.style.display = "none";
+    caja_trasera_register.style.opacity = "0";
+    caja_trasera_login.style.opacity = "1";
+
+
+}
