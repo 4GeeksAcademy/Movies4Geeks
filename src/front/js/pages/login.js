6dd0ffa66f334052bb286669f8073bda863953e5
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-06-23 19:32:18 +0000

    queda funcionando el desplazamiento de containers

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,42 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/login.css";
-import { register } from "./register";
 
 export const Login = () => {
-    return (
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleRegisterClick = () => {
+    setShowLoginForm(false);
+    document
+      .querySelector(".container__login-register")
+      .classList.add("formulario__register-active");
+  };
+
+  const handleLoginClick = () => {
+    setShowLoginForm(true);
+    document
+      .querySelector(".container__login-register")
+      .classList.remove("formulario__register-active");
+  };
+
+  return (
     <main>
-            <div className="container__todo">
-                <div className="caja__trasera">
-                    <div className="caja__trasera__login">
-                        <h3>¿Ya tienes una cuenta?</h3>
-                        <p>Inicia sesión para entrar en la página</p>
-                        <button id="btn__iniciar-sesion">Iniciar sesión</button>
-                    </div>
-                    <div className="caja__trasera-register">
-                        <h3>¿Aún no tienes una cuenta?</h3>
-                        <p>Regístrate y disfruta de las ventajas</p>
-                        <button id="btn__registrarse">Registrarse</button>
-                    </div>
-                </div>
-                {/* formulario de login y registro */}
-                <div className="container__login-register">
-                    <form action="" className="formulario__login">
-                        <h2>Iniciar Sesión</h2>
-                        <input type="text" placeholder="Correo Electrónico" /><br/>
-                        <input type="password" placeholder="Contraseña" /><br/>
-                        <button>Entrar</button>
-                    </form>
-                    <form action="" className="formulario__register">
-                        <h2>Regístrate</h2>
-                        <input type="text" placeholder="Nombre completo" /><br/>
-                        <input type="text" placeholder="Correo Electrónico" /><br/>
-                        <input type="text" placeholder="Nickname" /><br/>
-                        <input type="password" placeholder="Contraseña" />
-                        <button>Registrarse</button>
-                    </form>
-                </div>
-            </div>
-            <register /> 
+      <div className="container__todo">
+        <div className="caja__trasera">
+          <div className="caja__trasera__login">
+            <h3>¿Ya tienes una cuenta?</h3>
+            <p>Inicia sesión para entrar en la página</p>
+            <button id="btn__iniciar-sesion" onClick={handleLoginClick}>
+              Iniciar sesión
+            </button>
+          </div>
+          <div className="caja__trasera-register">
+            <h3>¿Aún no tienes una cuenta?</h3>
+            <p>Regístrate y disfruta de las ventajas</p>
+            <button id="btn__registrarse" onClick={handleRegisterClick}>
+              Registrarse
+            </button>
+          </div>
+        </div>
+        {/* formulario de login y registro */}
+        <div className="container__login-register">
+          <form
+            action=""
+            className={`formulario__login ${
+              showLoginForm ? "active" : "inactive"
+            }`}
+          >
+            <h2>Iniciar Sesión</h2>
+            <input type="text" placeholder="Correo Electrónico" />
+            <br />
+            <input type="password" placeholder="Contraseña" />
+            <br />
+            <button>Entrar</button>
+          </form>
+          <form
+            action=""
+            className={`formulario__register ${
+              showLoginForm ? "inactive" : "active"
+            }`}
+          >
+            <h2>Regístrate</h2>
+            <input type="text" placeholder="Nombre completo" />
+            <br />
+            <input type="text" placeholder="Correo Electrónico" />
+            <br />
+            <input type="text" placeholder="Nickname" />
+            <br />
+            <input type="password" placeholder="Contraseña" />
+            <button>Registrarse</button>
+          </form>
+        </div>
+      </div>
     </main>
-    );
-}
+  );
+};
--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,20 +1,35 @@
-document.getElementById("btn__registra")
-
-
-// Declaración de variables
-var container_login_register = document.querySelector(".container__login-register");
-var formulario_login = document.querySelector(".formulario__login");
-var formulario_register = document.querySelector(".formulario__register");
-var caja_trasera_login = document.querySelector(".caja_trasera-login");
-var caja_trasera_register = document.querySelector(".caja_trasera-register");
-
-
-export function register() {
-    formulario_register.style.display = "block";
-    container_login_register.style.left = "410px";
-    formulario_login.style.display = "none";
+document.addEventListener("DOMContentLoaded", function() {
+    var btnRegistrarse = document.getElementById("btn__registrarse");
+    btnRegistrarse.addEventListener("click", register);
+  
+    var btnIniciarSesion = document.getElementById("btn__iniciar-sesion");
+    btnIniciarSesion.addEventListener("click", iniciarSesion);
+  });
+  
+  function iniciarSesion() {
+    var formulario_login = document.querySelector(".formulario__login");
+    var formulario_register = document.querySelector(".formulario__register");
+    var container_login_register = document.querySelector(".container__login-register");
+    var caja_trasera_login = document.querySelector(".caja__trasera__login");
+    var caja_trasera_register = document.querySelector(".caja__trasera-register");
+  
+    formulario_register.style.display = "none";
+    container_login_register.classList.remove("formulario__register-active");
+    formulario_login.style.display = "block";
     caja_trasera_register.style.opacity = "0";
     caja_trasera_login.style.opacity = "1";
-
-
-}
+  }
+  
+  function register() {
+    var formulario_login = document.querySelector(".formulario__login");
+    var formulario_register = document.querySelector(".formulario__register");
+    var container_login_register = document.querySelector(".container__login-register");
+    var caja_trasera_login = document.querySelector(".caja__trasera__login");
+    var caja_trasera_register = document.querySelector(".caja__trasera-register");
+  
+    formulario_register.style.display = "block";
+    container_login_register.classList.add("formulario__register-active");
+    formulario_login.style.display = "none";
+    caja_trasera_register.style.opacity = "1";
+    caja_trasera_login.style.opacity = "0";
+  }
