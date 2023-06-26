06a966d4fcfd532175988790b21250c5d8646eaf
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-06-26 19:53:10 +0000

    26/06 ended

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -88,11 +88,19 @@ import logo from "../../img/logo_transparente.png"
 		</li>
       
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn bg-light mt-2" type="submit">Search</button>
-      </form>
-	  
+       		<form className="d-flex" >
+   <input className="form-control  bg-light input-form" type="search"  placeholder="Buscar" aria-label="Search" />
+   <span className="btn bg-light br-0 btn-submit" type="submit" >
+   <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search ml-1" viewBox="0 0 16 16">
+     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+   </svg>
+ </span>
+   
+</form>
+ <button classNameName="btn bg-light mr-1 br-0" type="submit" style={{ borderRadius:"10px", marginRight:"25px", border:"0" }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+ </svg></button>
+
 
     </div>
   </div>
