bcb092e1525974922b672b4704c33e4bc5f24bc4
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2023-07-06 02:09:51 +0000

    reviews

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,15 +4,11 @@ import {Login} from "./login";
 import "../../styles/footer.css"
 
 export const Footer = () => (
-<<<<<<< HEAD
-	<footer className="footer mt-auto py-3 text-center">
 		
-=======
 	<footer className="d-flex justify-content-center footer"  style={{ background: "#161616" }} >
 		<p className="text-light m-4">
 			Made by Javier, Johanna and Ruben.  
 		</p>
 		<Link to="/contactUs" className="btn bg-light m-3">Contact us</Link>
->>>>>>> main
 	</footer>
 );
