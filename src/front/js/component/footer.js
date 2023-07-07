3d8a2d30411a6e3916292aae95e7be4dd508ad06
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-07-07 14:13:11 +0000

    prueba

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,11 +4,14 @@ import {Login} from "./login";
 import "../../styles/footer.css"
 
 export const Footer = () => (
+
 		
+
 	<footer className="d-flex justify-content-center footer"  style={{ background: "#161616" }} >
 		<p className="text-light m-4">
 			Made by Javier, Johanna and Ruben.  
 		</p>
 		<Link to="/contactUs" className="btn bg-light m-3">Contact us</Link>
+
 	</footer>
 );
