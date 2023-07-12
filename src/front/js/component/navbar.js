62a4598b6a056c71605d07b238b570fdc2d64252
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-07-12 16:09:50 +0000

    maldito navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -17,26 +17,29 @@ import logo from "../../img/logo_transparente.png"
   
 
 <nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
+  <div className="container-fluid ms-3 mt-2 me-3">
     
-    <button className="navbar-toggler icon-navbar" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    
+    <div className="col-2">
+   <a href="/" className="li-logo"><img src={logo} className="navbar-brand logo " ></img></a>
+   </div>
+   <button className="navbar-toggler icon-navbar" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <svg xmlns="http://www.w3.org/2000/svg" className="svg-icon " height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
     </button>
-    
- 
     <div className="collapse navbar-collapse navbar-list" id="navbarSupportedContent">
+    
       <ul className="navbar-nav me-auto mb-2 mb-lg-0 pt-4 ul-navbar">
-        <li className="li-logo">
+        {/* <li className="li-logo">
       <Link to="../../">
-  <img src={logo} className="navbar-brand logo " ></img>
+  
   </Link>
-    </li>
+    </li> */}
 		
-    <li className="nav-item">
+    <li className="nav-item me-5">
       <Link to="/" className="nav-link text-light li-navbar" aria-current="page" >Peliculas</Link>
     </li>
     
-<li className="nav-item">
+<li className="nav-item me-5 ms-4">
   <Link to="/" className="nav-link text-light li-navbar " aria-aria-current="page" >Reviews</Link>
 </li>
 
@@ -47,7 +50,7 @@ import logo from "../../img/logo_transparente.png"
 </li> */}
   
   <li>
-  <form className="d-flex" >
+  <form className="d-flex ms-4" >
    <input className="form-control  bg-light input-form " type="search"  placeholder="Buscar" aria-label="Search" />
    <span className="btn bg-light br-0 btn-submit" type="submit" >
    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search ml-1" viewBox="0 0 16 16">
@@ -59,13 +62,13 @@ import logo from "../../img/logo_transparente.png"
 </li>
 
     </ul>
-    </div><li className="btn  ">
+    </div><div className="btn col-2 ">
 {isLoggedIn ? (
             <Link className="text-light text-decoration-none" to="/perfil">Mi perfil</Link>
           ) : (
             <Link className="text-light text-decoration-none btn-login" to="/userPage">Log in</Link>
           )}
-          </li>
+          </div>
   </div>
 </nav>
 </>
--- src/front/js/component/userPage.js
+++ src/front/js/component/userPage.js
@@ -10,52 +10,52 @@ import logo from "../../img/logo_transparente.png"
 
         return(
             <>
-            <form className="form-user">
+            <form className="form-user mt-3">
               <div className="row">
-  <div className="mb-3 col-6">
+  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="userName" className="form-label">Name</label>
     <input type="text" className="form-control" id="userName" placeholder="Ruben" disabled aria-describedby="emailHelp"/>
     
   </div>
 
-  <div className="mb-3 col-6">
+  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="userSurname" className="form-label">Surname</label>
     <input type="text" className="form-control" id="userSurame" placeholder="Garcia Gutierrez" disabled aria-describedby="emailHelp"/>
     
   </div>
   </div>  
   <div className="row">
-  <div className="mb-3 col-6">
+  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="password" className="form-label">New Password</label>
     <input type="password" className="form-control" id="password"  aria-describedby="emailHelp"/>
     
   </div>
   
-  <div className="mb-3 col-6">
+  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="newPassword" className="form-label">Confirm Password</label>
     <input type="password" className="form-control" id="newPassword"  />
     
   </div> 
   </div>
   <div className="row">
-   <div className="mb-3 col-6">
+   <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="newemail" className="form-label">New email</label>
     <input type="text" className="form-control" id="newemail" placeholder="a@a.com" />
     
   </div>
-  <div className="mb-3 col-6">
+  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="confirmemail" className="form-label">Confirm new email</label>
     <input type="password" className="form-control" id="confirmemail" placeholder="a@a.com"  />
     
   </div> 
   </div>
   <div className="row">
-   <div className="mb-3 col-6">
+   <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="exampleInputEmail1" className="form-label">Alias</label>
     <input type="text" className="form-control" id="exampleInputEmail1" placeholder="RubenGarGu" />
     
   </div>
-  <div className="mb-3 col-6">
+  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
     <label for="birthday" className="form-label">Birthday</label>
     <input type="date" className="form-control" id="birthday"/>
   </div>
