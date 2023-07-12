c24eb122d0683f17261fdf3f1d892bb00d8e9ec9
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-07-12 14:01:56 +0000

    almost done

--- src/front/js/component/userPage.js
+++ src/front/js/component/userPage.js
@@ -11,42 +11,62 @@ import logo from "../../img/logo_transparente.png"
         return(
             <>
             <form className="form-user">
-  <div className="mb-3">
+              <div className="row">
+  <div className="mb-3 col-6">
     <label for="userName" className="form-label">Name</label>
     <input type="text" className="form-control" id="userName" placeholder="Ruben" disabled aria-describedby="emailHelp"/>
     
   </div>
-  <div className="mb-3">
+
+  <div className="mb-3 col-6">
     <label for="userSurname" className="form-label">Surname</label>
     <input type="text" className="form-control" id="userSurame" placeholder="Garcia Gutierrez" disabled aria-describedby="emailHelp"/>
     
   </div>
-  <div className="mb-3">
+  </div>  
+  <div className="row">
+  <div className="mb-3 col-6">
     <label for="password" className="form-label">New Password</label>
     <input type="password" className="form-control" id="password"  aria-describedby="emailHelp"/>
     
   </div>
-  <div className="mb-3">
+  
+  <div className="mb-3 col-6">
     <label for="newPassword" className="form-label">Confirm Password</label>
     <input type="password" className="form-control" id="newPassword"  />
     
   </div> 
-   <div className="mb-3">
+  </div>
+  <div className="row">
+   <div className="mb-3 col-6">
+    <label for="newemail" className="form-label">New email</label>
+    <input type="text" className="form-control" id="newemail" placeholder="a@a.com" />
+    
+  </div>
+  <div className="mb-3 col-6">
+    <label for="confirmemail" className="form-label">Confirm new email</label>
+    <input type="password" className="form-control" id="confirmemail" placeholder="a@a.com"  />
+    
+  </div> 
+  </div>
+  <div className="row">
+   <div className="mb-3 col-6">
     <label for="exampleInputEmail1" className="form-label">Alias</label>
     <input type="text" className="form-control" id="exampleInputEmail1" placeholder="RubenGarGu" />
     
   </div>
-  <div className="mb-3">
+  <div className="mb-3 col-6">
     <label for="birthday" className="form-label">Birthday</label>
     <input type="date" className="form-control" id="birthday"/>
+  </div>
   </div>
   {/* Avatar */}
   <div className="mb-3 form-check">
     
   </div>
 <div className="button-user">
-  <button type="reset" className="btn btn-danger">Cancel</button>
-  <button type="submit" className="btn btn-success ml-1">Submit</button>
+  <button type="reset" className="btn cancel-user">Cancel</button>
+  <button type="submit" className="btn submit-user">Submit</button>
   </div>
 </form>
 
