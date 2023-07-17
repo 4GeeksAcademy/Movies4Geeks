265f2034aaab12e0c70dd507d2a3c30e1add8999
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-07-17 18:55:22 +0000

    17/07 end. Faltan funcionalidades

--- src/front/js/component/editUser.js
+++ src/front/js/component/editUser.js
@@ -38,7 +38,19 @@ import  "../../styles/editUser.css"
     // };
     
         return(
-            <>
+            
+            <div className="container userPage-container">
+        <div className="aside-user col-lg-3 col-12">
+          <p className="fw-bold">Ruben Garcia</p>
+          <Link to="/" className="personal-aside-edit-user"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+          </svg>Personal information</Link>
+          <Link to="/editUser" className="edit-user-active"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+          </svg>Edit information</Link>
+          
+        </div>
+        <div className="main-user col-lg-9 col-12">
             <form className="form-user mt-3">
   <div className="row">
     <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
@@ -101,8 +113,10 @@ import  "../../styles/editUser.css"
   <button type="submit" className="btn submit-user">Submit</button>
   </div>
 </form>
+</div>
+</div>
 
-</>
+
           
 
         );
--- src/front/js/component/userPage.js
+++ src/front/js/component/userPage.js
@@ -1,68 +1,63 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import  "../../styles/userPage.css"
+import "../../styles/userPage.css"
 
 
 
-	export const UserPage = () => {
-    const handleLogout = () => {
-      setIsLoggedIn(false);
-    };
+export const UserPage = () => {
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
 
-        return(
-            <>
-            <div className="container">
-            <div className="aside-user col-lg-3">
-              <p className="fw-bold">Ruben Garcia</p>
-              <p className="personal-aside-user"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
-</svg>Personal information</p>
-             <button className="btn user-control edit-user"><Link  to="../editUser"><button type="reset" className="btn"  ><svg xmlns="http://www.w3.org/2000/svg" className="icon-aside" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-             </svg><p>Edit</p></button></Link></button> 
-             <button className="btn  text-decoration-none user-control " onClick={handleLogout}><svg xmlns="http://www.w3.org/2000/svg" className=" icon-aside" width="16" height="16" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
-             <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
-             </svg><p>Cerrar sesión</p></button>
+  return (
+    <>
+      <div className="container userPage-container">
+        <div className="aside-user col-lg-3 col-12">
+          <p className="fw-bold">Ruben Garcia</p>
+          <Link to="/" className="personal-aside-user"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+          </svg>Personal information</Link>
+          <Link to="/editUser" className="edit-user"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+          </svg>Edit information</Link>
+          
+        </div>
+        <div className="main-user col-lg-9 col-12">
+          <form className="form-user">
+            <div className="row">
+              <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
+                <label for="userName" className="form-label">Name</label>
+                <input type="text" className="form-control input-user" id="userName" placeholder="Ruben" disabled aria-describedby="emailHelp" />
+              </div>
+              <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
+                <label for="userSurname" className="form-label">Surname</label>
+                <input type="text" className="form-control input-user" id="userSurname" placeholder="Garcia Gutierrez" disabled aria-describedby="emailHelp" />
+              </div>
             </div>
-            <div className="main-user col-lg-9">
-              <form className="form-user mt-3">
-                <div className="row">
-                  <div className="mb-3 col-lg-9 col-md-6 col-sm-12">
-                    <label for="userName" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="userName" placeholder="Ruben" disabled aria-describedby="emailHelp"/>
-                 </div>
-                 <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
-                   <label for="userSurname" className="form-label">Surname</label>
-                   <input type="text" className="form-control" id="userSurname" placeholder="Garcia Gutierrez" disabled aria-describedby="emailHelp"/>
-                 </div>
-                </div>  
 
-  
-  <div className="row">
-   <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
-    <label for="exampleInputEmail1" className="form-label">Alias</label>
-    <input type="text" className="form-control" id="exampleInputEmail1" disabled placeholder="RubenGarGu" />
-    
-  </div>
-  <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
-    <label for="birthday" className="form-label">Birthday</label>
-    <input type="text" className="form-control"  id="birthday" disabled placeholder="11/02/1988"/>
-  </div>
-  </div>
-  {/* Avatar */}
-  <div className="mb-3 form-check">
-    
-  </div>
-<div className="button-user">
-  <button type="reset" className="btn cancel-user">Cancel</button>
-  <button type="submit" className="btn submit-user">Submit</button>
-  </div>
-</form>
-</div>
-</div>
 
-</>
+            <div className="row">
+              <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
+                <label for="exampleInputEmail1" className="form-label">Alias</label>
+                <input type="text" className="form-control input-user" id="exampleInputEmail1" disabled placeholder="RubenGarGu" />
 
-        );
-    }
+              </div>
+              <div className="mb-3 col-lg-6 col-md-6 col-sm-12">
+                <label for="birthday" className="form-label">Birthday</label>
+                <input type="text" className="form-control input-user" id="birthday" disabled placeholder="11/02/1988" />
+              </div>
+            </div>
+            {/* Avatar */}
+            <div className="mb-3 form-check">
+
+            </div>
+           
+          </form>
+        </div>
+      </div>
+
+    </>
+
+  );
+}
